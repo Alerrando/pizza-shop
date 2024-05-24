9a6a8bc5240551d9c7f4c19dbf3d8ff469e5d59d
Alerrando <alerrando2@gmail.com>
2024-05-24 17:51:19 -0300

    feat: add account-menu component and theme dark

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | Pizza Shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | Pizza Shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
